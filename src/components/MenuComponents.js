b1f6f3572c0fca484c11009dae6d8b3f220a242c
sandipan898 <sandipan.das898@gmail.com>
2020-07-17 13:16:49 +0530

    creating navbar

---
+++ src/components/MenuComponents.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+  Media,
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardBody,
+  CardText
+} from "reactstrap";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedDish: null
+    };
+  }
+
+  render() {
+    const menu = this.props.dishes.map(dish => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-2">
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
